909c33015b372543d59cc9d73858204dd475e222
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2021-03-25 23:21:55 +0100

    feat: add utils to detect if git is really running the process

---
+++ src/utils/process.ts
@@ -0,0 +1,22 @@
+import findProcess from "find-process";
+
+type Process = ArrayElement<UnwrapPromise<ReturnType<typeof findProcess>>>;
+
+export class ProcessUtils {
+  static async isLaunchedByGit(): Promise<[boolean, string]> {
+    if (process.stdin.isTTY) {
+      return [false, "This command is mean to be used by git, but it is used in computer terminal (TTY)"];
+    }
+
+    const parentProcesses = await findProcess("pid", process.ppid);
+    const isProcessGit = (process: Process) => process.name === "git";
+    const hasGitInParentProcesses = parentProcesses.find(isProcessGit);
+
+    if (!hasGitInParentProcesses) {
+      const parentProcessesNames = parentProcesses.map((p) => p.name).join(", ");
+      return [false, `One of the parent processes should be "git", found ${parentProcessesNames}`];
+    }
+
+    return [true, ""];
+  }
+}
