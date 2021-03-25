5ed60983a24a16d8024fe38b54aea04cbac29afa
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2021-03-26 00:00:29 +0100

    feat: add diff command

---
+++ src/commands/diff.ts
@@ -0,0 +1,27 @@
+import { Command } from "@oclif/command";
+
+import * as fs from "fs";
+
+import { decode } from "../transforms/decode";
+import { ProcessUtils } from "../utils/process";
+
+export default class Diff extends Command {
+  static hidden = true;
+  static description = "(Git command) When diffing, encode files.";
+
+  static args = [
+    {
+      name: "path",
+      required: true,
+    },
+  ];
+
+  async run() {
+    const [isLaunchedByGit, processError] = await ProcessUtils.isLaunchedByGit();
+    !isLaunchedByGit && this.error(processError);
+
+    const { args } = this.parse(Diff);
+
+    fs.createReadStream(args.path).pipe(decode).pipe(process.stdout);
+  }
+}
