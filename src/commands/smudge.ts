7cecb5cce0061f8aac543b1a97c2dff06d814bfc
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2021-03-25 23:36:25 +0100

    feature: add clean and smudge command

--- src/commands/smudge.ts
+++ src/commands/smudge.ts
@@ -1,16 +1,16 @@
 import { Command } from "@oclif/command";
 
-import { encode } from "../transforms/encode";
+import { decode } from "../transforms/decode";
 import { ProcessUtils } from "../utils/process";
 
 export default class Clean extends Command {
   static hidden = true;
-  static description = "(Git command) When staging and committing, encode files.";
+  static description = "(Git command) When fetching and pulling, decode files.";
 
   async run() {
     const [isLaunchedByGit, failureReason] = await ProcessUtils.isLaunchedByGit();
     !isLaunchedByGit && this.error(failureReason);
 
-    process.stdin.pipe(encode).pipe(process.stdout);
+    process.stdin.pipe(decode).pipe(process.stdout);
   }
 }
