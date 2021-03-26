c6cbe793d76d887f1247f9cc97b30858d637e1cf
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2021-03-26 01:21:27 +0100

    feat: add a showAsStored command

---
+++ src/commands/showAsStored.ts
@@ -0,0 +1,23 @@
+import { Command } from "@oclif/command";
+
+import { Git } from "../git/git";
+
+export default class ShowAsStored extends Command {
+  static description = "Show the real content of a file, as git store it, before sending it to the server.";
+
+  static args = [
+    {
+      name: "path",
+      required: true,
+    },
+  ];
+
+  async run() {
+    const { args } = this.parse(ShowAsStored);
+    const { branches } = await Git.branch();
+    const activeBranch = branches.find((b) => b.current)?.name as string;
+    const { hash } = await Git.lsTree(activeBranch, args.path);
+    const { data } = await Git.catFile("blob", hash);
+    this.log(data);
+  }
+}
