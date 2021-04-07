b561c665cd9fe3455e96ce204bcedadbc8758370
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2021-04-07 19:28:49 +0200

    refactor: rename into spy-raw-file and add todos

--- src/commands/spy-raw-file.ts
+++ src/commands/spy-raw-file.ts
@@ -2,14 +2,17 @@ import { Command } from "@oclif/command";
 
 import { Git } from "../git/git";
 
+// TODO: document this
 // TODO: add debug https://oclif.io/docs/debugging
-export default class Spy extends Command {
+export default class SpyRawFile extends Command {
   static description = "Show the real content of a file, as git store it, before sending it to the server.";
 
-  static usage = "spy PATH";
+  static usage = "spy-raw-file PATH";
+
+  static aliases = ["spy"];
 
   static examples = [
-    `$ marauder spy e.txt
+    `$ marauder spy-raw-file e.txt
 
 ~(^marauder^)~
 8cf027a41e74a383046da101c4379a16
@@ -26,8 +29,9 @@ fe0ea581011cacc1650f77f5339f678b
     },
   ];
 
-  async run() {
-    const { args } = this.parse(Spy);
+  // TODO: handle errrors in the code
+  async run(): Promise<void> {
+    const { args } = this.parse(SpyRawFile);
     const { branches } = await Git.getCurrentBranches();
     const activeBranch = branches.find((b) => b.current)?.name as string;
     const { hash } = await Git.lsTree(activeBranch, args.path);
