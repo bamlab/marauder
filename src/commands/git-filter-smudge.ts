102c65300940ac6ec18494da5f836cba3a6fd409
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2021-04-07 19:13:43 +0200

    refactor: rename into git-filter-smudge and add todo

---
+++ src/commands/git-filter-smudge.ts
@@ -0,0 +1,35 @@
+import { Command } from "@oclif/command";
+
+import { decode } from "../transforms/decode";
+import { ProcessUtils } from "../utils/process";
+
+/**
+ * (Used by git, not for direct usage) Decode file
+ *
+ * When viewing file, decode the blob them before writing them in directory.
+ *
+ * @export
+ * @class GitFilterClean
+ * @extends {Command}
+ */
+export default class GitFilterSmudge extends Command {
+  static hidden = true;
+
+  // TODO: clean up description
+  static description = "(Git command) When fetching and pulling, decode files.";
+
+  async run(): Promise<void> {
+    // TODO: use monadic result struct
+    const [isNotTTY, ttyError] = await ProcessUtils.isLaunchedByGit();
+    // TODO: create an invariant(condition, error)
+    !isNotTTY && this.error(ttyError);
+
+    // TODO: use monadic result struct
+    const [isLaunchedByGit, processError] = await ProcessUtils.isLaunchedByGit();
+    // TODO: create an invariant(condition, error)
+    !isLaunchedByGit && this.error(processError);
+
+    // TODO: investigate how to display error in git (stderr ?)
+    process.stdin.pipe(decode).pipe(process.stdout);
+  }
+}
