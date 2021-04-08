a2e719bed7f33ee6b414c48ca3d2efc3d6af7b81
TychoTa <tycho.tatitscheff+ananke@gmail.com>
2021-04-08 18:26:32 +0200

    chore: remove useless code in crypto

--- src/services/crypto.ts
+++ src/services/crypto.ts
@@ -1,4 +1,4 @@
-import { CryptographyKey, SodiumPlus, X25519PublicKey } from "sodium-plus";
+import { CryptographyKey, SodiumPlus } from "sodium-plus";
 
 // TODO: add debug https://oclif.io/docs/debugging
 export class CryptoService {
@@ -6,59 +6,6 @@ export class CryptoService {
     const sodium = await SodiumPlus.auto();
     const key = await sodium.crypto_secretbox_keygen();
     return key.getBuffer().toString("base64");
-  }
-
-  static async generateClientKeypair(): Promise<string> {
-    const sodium = await SodiumPlus.auto();
-    const keypair = await sodium.crypto_kx_keypair();
-    return keypair.getBuffer().toString("base64");
-  }
-
-  static async generateServerKeypair(): Promise<string> {
-    const sodium = await SodiumPlus.auto();
-    const keypair = await sodium.crypto_kx_seed_keypair("marauder");
-    return keypair.getBuffer().toString("base64");
-  }
-
-  static async getPublicKey(storedKeyPair: string): Promise<string> {
-    const sodium = await SodiumPlus.auto();
-    const keypair = CryptographyKey.from(storedKeyPair, "base64");
-    const key = await sodium.crypto_box_publickey(keypair);
-    return key.getBuffer().toString("base64");
-  }
-
-  static async getServerExchangeKeypair(
-    serverStoredKeypair: string,
-    clientStoredPublicKey: string
-  ): Promise<[incoming: string, outgoing: string]> {
-    const sodium = await SodiumPlus.auto();
-    const serverKeypair = CryptographyKey.from(serverStoredKeypair, "base64");
-    const serverSecretKey = await sodium.crypto_box_secretkey(serverKeypair);
-    const serverPublicKey = await sodium.crypto_box_publickey(serverKeypair);
-    const clientPublicKey = X25519PublicKey.from(clientStoredPublicKey, "base64");
-    const [serverIncomingKey, serverOutgoingKey] = await sodium.crypto_kx_server_session_keys(
-      serverPublicKey,
-      serverSecretKey,
-      clientPublicKey
-    );
-    return [serverIncomingKey.getBuffer().toString("base64"), serverOutgoingKey.getBuffer().toString("base64")];
-  }
-
-  static async getClientExchangeKeypair(
-    clientStoredKeypair: string,
-    serverStoredPublicKey: string
-  ): Promise<[incoming: string, outgoing: string]> {
-    const sodium = await SodiumPlus.auto();
-    const clientKeyPair = CryptographyKey.from(clientStoredKeypair, "base64");
-    const clientSecretKey = await sodium.crypto_box_secretkey(clientKeyPair);
-    const clientPublicKey = await sodium.crypto_box_publickey(clientKeyPair);
-    const serverPublicKey = X25519PublicKey.from(serverStoredPublicKey, "base64");
-    const [clientIncomingKey, clientOutgoingKey] = await sodium.crypto_kx_client_session_keys(
-      clientPublicKey,
-      clientSecretKey,
-      serverPublicKey
-    );
-    return [clientIncomingKey.getBuffer().toString("base64"), clientOutgoingKey.getBuffer().toString("base64")];
   }
 
   static async cipherSecret(storedSecretKey: string, message: string): Promise<string> {
